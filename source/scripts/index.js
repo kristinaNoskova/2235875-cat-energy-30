e16733b5dc696bada5f2d1dc42d18a0f4d2bb928
Kristina <krisveronika282@gmail.com>
2024-01-03 23:05:19 +0300

    adaptive grids index.html

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const burgerButton = document.querySelector('.burger');
+const siteList = document.querySelector('.site-list');
+
+burgerButton.addEventListener('click', () => {
+  siteList.classList.toggle('site-list--open');
+  burgerButton.classList.toggle('burger--closed');
+});
